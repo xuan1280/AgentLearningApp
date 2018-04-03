adae53cb60a71c597d0f0c778bcd93e730d24253
johnny850807 <johnny850807@gmail.com>
2018-04-04 00:03:38 +0800

    init fragemnt

--- app/src/main/java/com/ood/clean/waterball/agentlearningapp/MainActivity.java
+++ app/src/main/java/com/ood/clean/waterball/agentlearningapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.ood.clean.waterball.agentlearningapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.container) FrameLayout sadasdsda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        TestFragment testFragment = TestFragment.getInstance("HAHA");
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, testFragment, "test")
+                .addToBackStack("test")
+                .commit();
     }
 }
---
+++ app/src/main/java/com/ood/clean/waterball/agentlearningapp/TestFragment.java
@@ -0,0 +1,80 @@
+package com.ood.clean.waterball.agentlearningapp;
+
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TestFragment extends Fragment {
+    private final static String DATA_KEY = "d1";
+    private String data;
+
+    public TestFragment(){
+        // Required empty public constructor
+    }
+
+    public static TestFragment getInstance(String data){
+        TestFragment testFragment = new TestFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(DATA_KEY, data);
+        testFragment.setArguments(bundle);
+        return testFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        data = getArguments().getString(DATA_KEY);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_test, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
