e3f1d78a33c2fe54e4e4e0cfb109ce8a6176dc40
xuan1280 <xuan1280@gmail.com>
2018-04-11 22:48:48 +0800

    create response model

---
+++ app/src/main/java/com/ood/clean/waterball/agentlearningapp/modles/viewmodels/ResponseModel.java
@@ -0,0 +1,31 @@
+package com.ood.clean.waterball.agentlearningapp.modles.viewmodels;
+
+public class ResponseModel<T> {
+    private int code;
+    private String message;
+    private T data;
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
