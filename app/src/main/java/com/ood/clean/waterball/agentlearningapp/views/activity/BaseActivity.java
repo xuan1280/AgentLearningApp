41925ba0ad854bb1520e883008eadd1b1a503c72
joanna <xuan1280@gmail.com>
2018-04-19 01:06:57 +0800

    pull

--- app/src/main/java/com/ood/clean/waterball/agentlearningapp/views/activity/BaseActivity.java
+++ app/src/main/java/com/ood/clean/waterball/agentlearningapp/views/activity/BaseActivity.java
@@ -1,33 +1,40 @@
 package com.ood.clean.waterball.agentlearningapp.views.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.ood.clean.waterball.agentlearningapp.ActivitiesFragment;
 import com.ood.clean.waterball.agentlearningapp.R;
 import com.ood.clean.waterball.agentlearningapp.modles.entities.User;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private final static String TAG = "BaseActivity";
     private List<Fragment> pages;
     private User user;
     private PagerAdapter pagerAdapter;
     private String[] options;
+    @BindView(R.id.navigationView) NavigationView navigationView;
+    @BindView(R.id.drawerlayout) DrawerLayout drawlayout;
     @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.tabs) TabLayout tabLayout;
     @BindView(R.id.pager) ViewPager viewPager;
@@ -47,6 +54,8 @@ public class BaseActivity extends AppCompatActivity {
         Log.d(TAG, user.getName() + " sign in successfully.");
         initPages();
         initTabs();
+        setupActionBarDrawerToggle();
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void initPages() {
@@ -58,6 +67,42 @@ public class BaseActivity extends AppCompatActivity {
 
     private void initTabs() {
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == android.R.id.home)
+        {
+            drawlayout.openDrawer(GravityCompat.START);
+            return true ;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupActionBarDrawerToggle() {
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawlayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        drawlayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        item.setChecked(true);
+        drawlayout.closeDrawers();
+        return true;
     }
 
 
